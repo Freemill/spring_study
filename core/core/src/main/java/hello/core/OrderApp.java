d2d5da30df6d303bfebd49dc943108b24952a395
Freemill <mongddangand@likelion.org>
2022-07-13 23:27:32 +0900

    Last Commit

--- core/core/src/main/java/hello/core/OrderApp.java
+++ core/core/src/main/java/hello/core/OrderApp.java
@@ -4,16 +4,20 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        Member member = new Member(1L, "memberA", Grade.VIP);
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
-
-        Order orderA = orderService.createOrder(1L, "orderA", 10000);
-
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
 
     }
 }
